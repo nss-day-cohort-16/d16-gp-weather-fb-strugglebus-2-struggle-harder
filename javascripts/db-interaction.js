a1be1664d88bd0bc81ba3797710777fad9bcf596
Abby Moore <abbyleighanne@gmail.com>
2016-11-29 11:56:58 -0600

    output

--- javascripts/db-interaction.js
+++ javascripts/db-interaction.js
@@ -6,13 +6,41 @@ let firebase = require("./firebaseConfig");
 function getWeather(zip) {
   return new Promise(function(resolve, reject) {
     $.ajax({
-      url: `http://api.openweathermap.org/data/2.5/weather?zip=${zip}&APPID=c10648665493f00d731df750ee9b0436
+      url: `http://api.openweathermap.org/data/2.5/weather?zip=${zip}&APPID=
 `
     }).done(function(weatherData){
+      console.log(weatherData);
       resolve(weatherData);
     });
   });
 }
 
-module.exports = {getWeather}; 
+function getThreeDayWeather(zipcode){
+	return new Promise((resolve, reject) => {
+		$.ajax({
+			url:`http://api.openweathermap.org/data/2.5/forecast/daily?zip=${zipcode}&cnt=3&APPID=`,
+			dataType: "json"
+		}).done(function(data) {
+			resolve(data);
+		}).fail((error) => {
+			reject(error);
+		});
+	});
+}
+
+function getSevenDayWeather(zipcode){
+	return new Promise((resolve, reject) => {
+		$.ajax({
+			url:`http://api.openweathermap.org/data/2.5/forecast/daily?zip=${zipcode}&cnt=7&APPID=`,
+			dataType: "json"
+		}).done(function(data) {
+			resolve(data);
+		}).fail((error) => {
+			reject(error);
+		});
+	});
+}
+
+
+module.exports = {getWeather, getThreeDayWeather, getSevenDayWeather}; 
 
